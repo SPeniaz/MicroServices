922fb5845283b1a2bd5b724c80385a41d8f9d0e0
S.Penyaz <work.spenyaz@gmail.com>
2023-03-01 13:29:35 +0100

    command-srvc: added-grpc-client

---
+++ CommandService/SyncDataServices/Grpc/PlatformDataClient.cs
@@ -0,0 +1,42 @@
+using AutoMapper;
+using CommandService.Models;
+using CommandService.SyncDataServices.Grpc.Interfaces;
+using Grpc.Net.Client;
+using PlatformService;
+
+namespace CommandService.SyncDataServices.Grpc
+{
+    public class PlatformDataClient : IPlatformDataClient
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+
+        public PlatformDataClient(
+            IConfiguration configuration,
+            IMapper mapper
+        )
+        {
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<Platform> ReturnAllPlatforms()
+        {
+            System.Console.WriteLine($"--> Calling GRPC service {_configuration["GrpcPlatform"]} ..");
+            var channel = GrpcChannel.ForAddress(_configuration["GrpcPlatform"]);
+            var client = new GrpcPlatform.GrpcPlatformClient(channel);
+            var request = new GetAllRequest();
+
+            try
+            {
+                var reply = client.GetAllPlatforms(request);
+                return _mapper.Map<IEnumerable<Platform>>(reply.Platform);
+            }
+            catch(Exception ex)
+            {
+                System.Console.WriteLine($"--> Could not call Grpc server: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
